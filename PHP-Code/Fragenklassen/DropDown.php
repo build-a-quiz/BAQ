8c73715fd8c775fb7136c943763b3eb3c783db1d
countess-asteria <Theresa_Alt@hotmail.de>
2021-11-09 08:29:03 +0100

    Auslagerung des Headers abhängig vom Fragetyp

--- PHP-Code/Fragenklassen/DropDown.php
+++ PHP-Code/Fragenklassen/DropDown.php
@@ -7,15 +7,8 @@ class DropDown extends Question{
         $this->setType($type);
     }
     function setQuestion($question, $answers, $solution){
-        echo "<div>
-                <br>
-                <h4 class='display-3'>
-                    <small class='text-muted'>
-                        Wählen Sie die korrekte Antwort aus
-                    </small>
-                </h4>
-                <br>
-                <form>
+        $this->getHeader($this->getType());
+        echo "<form>
                     <h4>{$question}</h4>
                     <div>
                         <select class='form-select' aria-label='Bitte Anwort auswählen'>
--- PHP-Code/Fragenklassen/Question.php
+++ PHP-Code/Fragenklassen/Question.php
@@ -29,4 +29,27 @@ abstract class Question {
     public function getType(): string{
         return $this->type;
     }
+    public function getHeader($type){
+        if($type == "MultipleChoice" || $type == "DropDown"){
+            echo "<div>
+                <br>
+                <h4 class='display-3'>
+                    <small class='text-muted'>
+                        Wählen Sie die korrekte Antwort aus
+                    </small>
+                </h4>
+                <br>". PHP_EOL;
+        } elseif ($type == "FreeText"){
+            echo "<div>
+                <br>
+                <h4 class='display-3'>
+                    <small class='text-muted'>
+                        Tragen Sie die korrekte Antwort in das Eingabefeld ein
+                    </small>
+                </h4>
+                <br>".PHP_EOL;
+        } else {
+            echo "<h4 class='display-3'> Kein gültiger Klassentyp mitgegeben</h4>".PHP_EOL;
+        }
+    }
 }
