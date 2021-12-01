ce565384c9e17e312a33777b949c314a1430e64d
rohrdani <84032500+rohrdani@users.noreply.github.com>
2021-12-01 08:18:12 +0100

    alten Stand bei MultipleChoice.php entfernt

--- PHP-Code/Fragenklassen/MultipleChoice.php
+++ PHP-Code/Fragenklassen/MultipleChoice.php
@@ -1,33 +1,4 @@
 <?php
-/* //Stand: 29.11.2021
-class MultipleChoice extends Question{
-    private int $counter = 0;
-	static $mc_fragen_counter = 0;
-
-    function __construct(){
-        $type ="MultipleChoice";
-        $this->setType($type);
-		MultipleChoice::$mc_fragen_counter++;
-    }
-    function buildQuestion($question, $answers){
-       // $this->getHeader($this->getType());
-        echo "
-                    <h4 class='display-8'>{$question}</h4>
-                    <div>".PHP_EOL;
-                    foreach ($answers as $answer){
-                        $count = $this->counter++;
-                        echo '
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="switch" name="Auswahl_MC'.MultipleChoice::$mc_fragen_counter.$count.'" id="option'.$count.'" value="'.$count.'">
-                                <label class="form-check-label" for="option'.$count.'">' .$answer. '</label>
-                            </div>' .PHP_EOL;
-                    }
-                    echo "</div>" . PHP_EOL;
-    }
-}
-*/
-
-//Stand: 30.11.201
 class MultipleChoice extends Question{
     private int $counter = 0;
     static $mc_fragen_counter = 1;
