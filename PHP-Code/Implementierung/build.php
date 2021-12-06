5d9208033fb631713a7e27085c898c51a2a9cad8
Flo <flo.vogl@protonmail.com>
2021-12-06 15:07:23 +0100

    build-Seite fertig gestellt:   - Quiz wird komplett in JavaScript gebaut und mittels HTTP-Post an den Server geschickt   - Verarbeitung am Server erfolgt mittels send.php

--- PHP-Code/Implementierung/build.php
+++ PHP-Code/Implementierung/build.php
@@ -1,18 +1,10 @@
 <?php
 
-// write quiz to databse
-//if (isset($_POST['submit'])) {
-//
-//}
-
 // just for testing set username and userid
 $user = "rassil0n";
 $userid = "1337";
 
 require_once '../config/config.php';
-$freeTextQuestion = new FreeText();
-$multipleChoiceQuestion = new MultipleChoice();
-$dropDownQuestion = new DropDown();
 
 // ugly way to pass php-variable for javascript
 echo "<input type='hidden' id='username' value='$user'>";
@@ -25,12 +17,13 @@ echo '<!DOCTYPE html>
     <title>BAQ - Let\'s Build</title>
     <link rel="shortcut icon" href="../../resources/BAQ.PNG" />
     
+    <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@sweetalert2/themes@5.0.8/bootstrap-4/bootstrap-4.min.css">
           
+     <!-- Javascript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.2.1/sweetalert2.min.js" integrity="sha512-qsog2Un5vHgtBLsgIIcZcfcRNxUXAswH2TxciIVDcB/reXRm1hFyH5Eb1ubQDUK149uK2HzuC0HFcqtSY5F1gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -51,128 +44,57 @@ echo "
                 </h1>
             <br>
           " . PHP_EOL;
-
-//$multipleChoiceQuestion->buildQuestion("Welcher Wissenschaftler sollte keine lebende Katze in die Finger bekommen?", ["Albert Einstein", "Robert Oppenheimer", "MarieCurie", "Max Planck", "Erwin Schrödinger"]);
-//echo "<br><br><hr><br>" . PHP_EOL;
-//$dropDownQuestion->buildQuestion("Was ist die höchstwertige technische Entwicklung auf Basis des Tunneleffekts ", ["Hochfrequenz-Halbleiter", "Cooper-Paare", "Gleichmäßig verteilter Käse auf Nachos"]);
-//echo "<hr>" . PHP_EOL;
-//$freeTextQuestion->buildQuestion("Wie wird ein Qubit auf der Bloch-Kugel dargestellt?", ["Vektor"]);
-//$multipleChoiceQuestion->setQuestion("Hello World");
-//echo $multipleChoiceQuestion->getQuestion();
-
 ?>
 
     <script src="funcs.js"></script>
 
-    <script>
-        // function to send the question to the server
-
-        let allQ = [];
-        let allU = [];
-        function send() {
-
-            // get information about quiz and players
-            let creator = document.getElementById('userid').value
-            let players = allU.toString()
-            let quizname = document.getElementById('quizname').value
-
-            // generate new request
-            let req = new XMLHttpRequest();
-            req.open("POST", 'send.php');
-            req.setRequestHeader("creator", creator);
-            req.setRequestHeader("players", players);
-            req.setRequestHeader('quizname', quizname);
-
-            // send request to server
-            req.send(JSON.stringify(allQ));
-            console.log(allQ)
-            // location.reload();
-
-            if(quizname) {
-                swal.fire({
-                    icon: 'success',
-                    title: "Quiz " + quizname + " wurde erfolgreich erstellt und in der Datenbank gespeichert",
-                    confirmButtonText: 'Alles klar!',
-                }).then((result) => {
-                        if (result.isConfirmed) {
-                            document.getElementById('quizname').value=''
-                            location.reload()
-                        }
-                    })
-            }
-            else {
-                swal.fire({
-                    icon: 'warning',
-                    title: "Es wurde kein Quizname vergeben!"
-                })
-            }
-
-            // $.ajax({
-                //     type: 'POST',
-                //     url: 'send.php',
-                //     data: fnord,
-                //     success: function () {
-                //         location.reload();
-                //     }
-                // });
-        }
-
-        // function to generate new Free-Text Question
-
-    </script>
-
     <hr>
     <div class="input-group input-group-default mb-2">
-    <span class="input-group-text" id="inputGroup-sizing-default">Quizname</span>
-    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" id="quizname">
+        <span class="input-group-text" id="inputGroup-sizing-default">Quizname</span>
+        <input type="text" class="form-control" aria-label="Sizing example input"
+               aria-describedby="inputGroup-sizing-sm" id="quizname">
     </div>
     <div class="btn-group">
-    <div class="dropdown">
-        <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Frage hinzufügen
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-            <button class="dropdown-item" type="button" onclick="addFreeText()">Freitext</button>
-            <button class="dropdown-item" type="button" onclick="addDropDown()">Dropdown</button>
-            <button class="dropdown-item" type="button" onclick="addMultipleChoice()">Multiple Choice</button>
+        <div class="dropdown">
+            <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                Frage hinzufügen
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <button class="dropdown-item" type="button" onclick="addFreeText()">Freitext</button>
+                <button class="dropdown-item" type="button" onclick="addDropDown()">Dropdown</button>
+                <button class="dropdown-item" type="button" onclick="addMultipleChoice()">Multiple Choice (SA)</button>
+                <button class="dropdown-item" type="button" onclick="addMultipleChoiceMA()">Multiple Choice (MA)
+                </button>
+            </div>
         </div>
-    </div>
         <button class="btn"></button>
-<button class="btn btn-warning" onclick="addUser()">Quizzer hinzufügen</button>
-</div>
+        <button class="btn btn-warning" onclick="addUser()">Quizzer hinzufügen</button>
+    </div>
 
     <hr>
 
     <br><br>
-<h6 class="display-6">Quizzer</h6> <h6 class="subtitle text-muted">Wer soll an dem Quiz teilnehmen können</h6>
-<table class="table table-responsive table-borderless table-sm" id="players">
-</table>
+    <!-- Preview the quizzer -->
+    <h6 class="display-6">Quizzer</h6> <h6 class="subtitle text-muted">Wer soll an dem Quiz teilnehmen können</h6>
+    <table class="table table-responsive table-borderless table-sm" id="players">
+    </table>
 
     <br><br>
 
-<h6 class="display-6">Preview des Quiz</h6> <h6 class="subtitle text-muted">(Achtung kein voller Funktionsumfang)</h6>
+    <h6 class="display-6">Preview des Quiz</h6> <h6 class="subtitle text-muted">(Achtung kein voller
+    Funktionsumfang)</h6>
     <!--   Preview the quiz-->
     <div id="preview">
     </div>
 
-
-<!--    <form action="send.php" method="post">-->
-
-        <br> <br>
-<!--        <hr>-->
-        <br>
-        <div class="SubmitButtonBox, text-center">
-            <button onclick="send()" class="btn btn-info btn-lg">Quiz erstellen</button>
-        </div>
-        <br>
-<!--        <hr>-->
-        <br> <br>
-<!--    </form>-->
-
-<button onclick="send()">Test send</button>
+    <br> <br><br>
+    <div class="SubmitButtonBox, text-center">
+        <button onclick="send()" class="btn btn-info btn-lg">Quiz erstellen</button>
+    </div>
+    <br><br> <br>
 
 <?php
-//Helper::printButton();
 echo '
                </div>
             </div>
--- PHP-Code/Implementierung/funcs.js
+++ PHP-Code/Implementierung/funcs.js
@@ -1,27 +1,74 @@
-async function addUser(){
-    const {value: user} = await Swal.fire({
-        title: 'Bitte Username eingeben:',
-        // icon: 'question',
-        input: 'text',
-        showCancelButton: true,
-        inputValidator: (value) => {
-            if (!value) {
-                return 'Schreiben oder Abbrechen!'
+/*
+ * funcs.js
+ * General purpose javascript.
+ * The quiz is entirely built in the browser-session via javascript.
+ * This is done for the sake of not having fragments on the server.
+ */
+
+// global variables
+let allQ = [];
+let allU = [];
+
+// function to send the question to the server
+function send() {
+
+    // get information about quiz and players
+    let creator = document.getElementById('userid').value
+    let players = allU.toString()
+    let quizname = document.getElementById('quizname').value
+
+    // generate new request
+    let req = new XMLHttpRequest();
+    req.open("POST", 'send.php');
+    req.setRequestHeader("creator", creator);
+    req.setRequestHeader("players", players);
+    req.setRequestHeader('quizname', quizname);
+
+    // send request to server
+    req.send(JSON.stringify(allQ));
+    console.log(allQ)
+
+    if (quizname) {
+        swal.fire({
+            icon: 'success',
+            title: "Quiz " + quizname + " wurde erfolgreich erstellt und in der Datenbank gespeichert",
+            confirmButtonText: 'Alles klar!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('quizname').value = ''
+                location.reload()
             }
-        }
-    });
-
-    allU.push(user)
-
-    if(user){
-
-        let playersTable = document.getElementById('players').innerHTML;
-        document.getElementById('players').innerHTML=playersTable + "<tr><td>&bullet; " + user + "</td></tr>";
+        })
+    } else {
+        swal.fire({
+            icon: 'warning',
+            title: "Es wurde kein Quizname vergeben!"
+        })
     }
-
-    console.log(allU)
 }
+async function addUser(){
+        const {value: user} = await Swal.fire({
+            title: 'Bitte Username eingeben:',
+            // icon: 'question',
+            input: 'text',
+            showCancelButton: true,
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'Schreiben oder Abbrechen!'
+                }
+            }
+        });
 
+        allU.push(user)
+
+        if(user){
+
+            let playersTable = document.getElementById('players').innerHTML;
+            document.getElementById('players').innerHTML=playersTable + "<tr><td>&bullet; " + user + "</td></tr>";
+        }
+
+        console.log(allU)
+    }
 async function addFreeText() {
 
     const {value: question} = await Swal.fire({
@@ -62,7 +109,7 @@ async function addFreeText() {
 
     // write question to temp array and print on preview
     if(question && solution && points) {
-        allQ.push({"id":1,"type":"freetext","question":question,"solution":solution,"points":points})
+        allQ.push({"id":1,"type":"FreeText","question":question,"solution":solution,"points":points})
         console.log(allQ);
 
         // update preview div
@@ -74,8 +121,6 @@ async function addFreeText() {
                 </div>"
     }
 }
-
-
 async function addDropDown(){
     const {value: question} = await Swal.fire({
         title: 'Bitte DropDown-Frage eingeben:',
@@ -119,7 +164,12 @@ async function addDropDown(){
             }
         }
     });
-    solution = answers.indexOf(solution);
+
+    // make answers lowercase
+    let lowercaseAnswers = answers.map(ans => ans.toLowerCase())
+
+    // get right answer
+    solution = lowercaseAnswers.indexOf(solution.toLowerCase());
 
     const {value: points} = await Swal.fire({
         title: 'Wie viel Punkte gibt es auf die Frage?',
@@ -134,9 +184,9 @@ async function addDropDown(){
     })
 
     // write question to temp array and print on preview
-    if(question && answers && solution && points) {
+    if(question && answers && solution >= 0 && points) {
 
-        allQ.push({"id":1,"type":"dropdown","question":question,"answers":answers,"solution":solution,"points":points})
+        allQ.push({"id":1,"type":"DropDown","question":question,"answers":answers,"solution":solution,"points":points})
         console.log(allQ);
 
         // update preview div
@@ -183,10 +233,74 @@ async function addMultipleChoice(){
         }
     });
 
-    const { value: solutions } = await Swal.fire({
+    let {value: solution} = await Swal.fire({
         title: answers,
+        icon: 'question',
+        input: 'text',
+        inputLabel : 'Bitte richtige Antwort angeben:',
+        showCancelButton: true,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Schreiben oder Abbrechen!'
+            }
+        }
+    });
+
+    let lowsercaseAnswers = answers.map(ans => ans.toLowerCase())
+    solution = lowsercaseAnswers.indexOf(solution.toLowerCase());
+
+    console.log(solution)
+
+
+    const {value: points} = await Swal.fire({
+        title: 'Wie viel Punkte gibt es auf die Frage?',
+        icon: 'question',
+        input: 'range',
+        inputAttributes: {
+            min: 1,
+            max: 10,
+            step: 1
+        },
+        inputValue: 1
+    });
+
+    if(question && answers && solution >= 0 && points) {
+
+        allQ.push({"id":1,"type":"MultipleChoice","question":question,"answers":answers,"solution":solution,"points":points})
+        console.log(allQ);
+
+        // update preview div
+        let preview = document.getElementById('preview').innerHTML;
+        document.getElementById('preview').innerHTML=preview +"<br><br><hr><br>\
+                <h4 class='display-8'>"+question+" ("+points+" Punkte)</h4>\
+                <div class='col-xs-3'>\
+            <div class='form-check form-sclassName'>\
+                <input class='form-check-input' type='radio'>\
+                <label class='form-check-label'>Anwort-Möglichkeit</label>\
+            </div>\
+            <div class='form-check form-sclassName'>\
+                <input class='form-check-input' type='radio'>\
+                <label class='form-check-label'>Anwort-Möglichkeit</label>\
+            </div>"
+    }
+}
+async function addMultipleChoiceMA(){
+
+    const {value: question} = await Swal.fire({
+        title: 'Bitte Multiple-Choice-Frage eingeben:',
+        icon: 'question',
+        input: 'text',
+        showCancelButton: true,
+        inputValidator: (value) => {
+            if (!value) {
+                return 'Schreiben oder Abbrechen!'
+            }
+        }
+    });
+
+    const { value: answers } = await Swal.fire({
+        title: 'Antwortmöglichkeiten definieren',
         html:
-            '<b>Bitte die richtigen Antworten angeben: </b><br>' +
             '<input id="swal-input1" class="swal2-input">' +
             '<input id="swal-input2" class="swal2-input">'+
             '<input id="swal-input3" class="swal2-input">'+
@@ -202,6 +316,39 @@ async function addMultipleChoice(){
         }
     });
 
+    let lowercaseAnswers = answers.map(name => name.toLowerCase())
+
+    console.log(lowercaseAnswers)
+
+    let {value: solutions} = await Swal.fire({
+        title: answers,
+
+        html:
+            '<b>Bitte die richtigen Lösungen angeben:</b>' +
+            '<input id="swal-input1" class="swal2-input">' +
+            '<input id="swal-input2" class="swal2-input">'+
+            '<input id="swal-input3" class="swal2-input">'+
+            '<input id="swal-input4" class="swal2-input">',
+        focusConfirm: false,
+        preConfirm: () => {
+            return [
+                document.getElementById('swal-input1').value,
+                document.getElementById('swal-input2').value,
+                document.getElementById('swal-input3').value,
+                document.getElementById('swal-input4').value
+            ]
+        }
+    });
+
+    let solNums =[]
+
+    let lowercaseSolutions = solutions.map(sol => sol.toLowerCase())
+    lowercaseSolutions.forEach(sol => {
+        let num = lowercaseAnswers.indexOf(sol)
+        if (num >= 0)
+            solNums.push(num)
+    })
+
     const {value: points} = await Swal.fire({
         title: 'Wie viel Punkte gibt es auf die Frage?',
         icon: 'question',
@@ -214,9 +361,9 @@ async function addMultipleChoice(){
         inputValue: 1
     });
 
-    if(question && answers && solutions && points) {
+    if(question && answers && solNums && points) {
 
-        allQ.push({"id":1,"type":"dropdown","question":question,"answers":answers,"solution":solutions,"points":points})
+        allQ.push({"id":1,"type":"MultipleChoice","question":question,"answers":answers,"solution":solNums,"points":points})
         console.log(allQ);
 
         // update preview div
