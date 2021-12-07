8582e76db3e260c6c4839049430e3181c9c04dcc
countess-asteria <80770471+countess-asteria@users.noreply.github.com>
2021-12-07 12:19:45 +0100

    Anpassen der Anzeige

--- PHP-Code/Login/reset-password.php
+++ PHP-Code/Login/reset-password.php
@@ -79,27 +79,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
+        .wrapper{ width: 560px; padding: 20px; }
     </style>
 </head>
 <body>
 <div class="wrapper">
-    <h2>Reset Password</h2>
-    <p>Please fill out this form to reset your password.</p>
+    <h2>Passwort zurücksetzen für <?php echo "$username"?></h2>
+    <p>Biite Formular ausfüllen, um das das Passwort zurücksetzen</p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group">
-            <label>New Password</label>
+            <label>Neues Passwort</label>
             <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
             <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
         </div>
         <div class="form-group">
-            <label>Confirm Password</label>
+            <label>Passwort bestätigen</label>
             <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
             <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
         </div>
         <div class="form-group">
-            <input type="submit" class="btn btn-primary" value="Submit">
-            <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
+            <input type="submit" class="btn btn-primary" value="Bestätigen">
+            <a class="btn btn-link ml-2" href="welcome.php">Abbrechen</a>
         </div>
     </form>
 </div>
